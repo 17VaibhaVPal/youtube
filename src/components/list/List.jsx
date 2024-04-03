eed2864e2c851e345a0a36aad1f03d5434392f04
17VaibhaVPal <vaibhavpal17sept@gmail.com>
2024-04-03 09:17:52 +0530

    added arrow feature for items

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,14 +1,48 @@
-import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from "@material-ui/icons"
+import { ArrowBackIosOutlined, ArrowForwardIosOutlined, Directions } from "@material-ui/icons"
 import "./list.scss"
 import  ListItem  from "../listItems/ListItems";
+import { useRef, useState } from "react";
 
 export default function List() {
+  const[isMoved,setIsMoved] = useState(false)
+  const[slideNumber,setSlideNumber]=useState(0);
+  //initially not clicked so false
+
+
+  const listRef =useRef() 
+  //The listRef variable will hold a   reference  to   a DOM element, typically used to access and manipulate the DOM element imperatively.
+
+  const handleClick =(direction) =>{
+
+    setIsMoved(true) // when clicked, it will become true 
+
+    // function named **handleClick**, which takes a **direction** parameter. This function will be responsible for handling click events and changing the position of the referenced DOM element based on the specified direction.
+
+    let distance =listRef.current.getBoundingClientRect().x-50  
+    //calculates the distance of the referenced DOM element from the left edge of the viewport. 
+     //It uses the getBoundingClientRect() method to obtain the position and size of the DOM element relative to the viewport,
+    if (direction=== "left" && slideNumber>0){
+      setSlideNumber(slideNumber-1);
+      listRef.current.style.transform =`translateX(${230 
+      + distance}px)`
+
+    }
+    if (direction=== "right" && slideNumber<5){
+      setSlideNumber(slideNumber+1);
+      listRef.current.style.transform =`translateX(${-230 
+      + distance}px)`
+
+    }
+    //transform CSS property is used to translate the element horizontally.
+
+  }
   return (
     <div className="list">
         <span className="listTitle">Continue to Watch</span>
         <div className="wrapper">
-            <ArrowBackIosOutlined className="SliderArrow left"/>
-            <div className="container">
+            <ArrowBackIosOutlined className="SliderArrow left" onClick={()=>handleClick("left")}
+            style={{display: !isMoved && "none"}}/>
+            <div className="container" ref={listRef}>
                 <ListItem/>
                 <ListItem/>
                 <ListItem/>
@@ -20,7 +54,7 @@ export default function List() {
                 <ListItem/>
                 <ListItem/>
             </div>
-            <ArrowForwardIosOutlined className="SliderArrow right"/>
+            <ArrowForwardIosOutlined className="SliderArrow right"onClick={()=>handleClick("right")}/>
         </div>
 
 
--- src/components/listItems/ListItems.jsx
+++ src/components/listItems/ListItems.jsx
@@ -1,9 +1,27 @@
+import { Add, PlayArrow, ThumbDownAltOutlined, ThumbUpAltOutlined } from "@material-ui/icons"
 import "./listItems.scss"
 
 export default function ListItems() {
   return (
     <div className="listItems">
-        Item
+        <img src="https://rukminim2.flixcart.com/image/850/1000/xif0q/poster/p/q/x/medium-john-wick-movie-framed-poster-for-room-office-hollywood-original-imagn5h9czvhd8ca.jpeg?q=90&crop=false" alt="" />
+        <div className="itemInfo">
+          <div className="icons">
+            <PlayArrow/>
+            <Add/>
+            <ThumbUpAltOutlined/>
+            <ThumbDownAltOutlined/>
+          </div>
+          <div className="ItemInfoTop">
+            <span>1 hour 14 mins</span>
+            <span className="limit">16+</span>
+            <span>1999</span>
+          </div>
+          <div className="desc">
+          John Wick is a retired assassin who returns back to his old ways after a group of Russian gangsters steal his car and kill a puppy which was given to him by his late wife Helen.John Wick takes his fight against the High Table global as he seeks out more powerful players in the underworld from different countries.
+          </div>
+          <div className="genre">Action</div>
+        </div>
     </div>
   )
 }
