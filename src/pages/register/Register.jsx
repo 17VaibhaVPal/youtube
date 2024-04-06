78da61b1d887f448ddc402e1d4ed812d2549119e
17VaibhaVPal <vaibhavpal17sept@gmail.com>
2024-04-06 11:08:01 +0530

    added email and password storing feature

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,8 +1,21 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./register.scss"
 
 export default function Register() {
-    
+    const[email,setEmail]=useState(""); // to store the email entered
+    const[password,setPassword]=useState("");// to store password
+
+   const emailRef=useRef();
+   const passwordRef=useRef();
+
+const handleStart = ()=>{
+    setEmail(emailRef.current.value)
+}
+
+
+const handleFinish = ()=>{
+    setPassword(passwordRef.current.value)
+}
   return (
     <div className="register">
           <div className="top">
@@ -22,10 +35,24 @@ export default function Register() {
               <p>
                 Ready to watch ? Enter your e-mail to create or restart your membership
               </p>
-              <div className="input">
-                <input type="email" placeholder="email address" />
-                <button className="registerButton">Get Started</button>
-              </div>
+
+
+              {!email ? 
+              (<div className="input">
+                <input type="email" placeholder="email address " ref={emailRef} />
+                <button className="registerButton" 
+                onClick={handleStart}>
+                    Get Started
+                    </button>
+              </div>) : 
+              (<form className="input">
+                <input type="password" placeholder="password " ref={passwordRef} />
+                <button className="registerButton"
+                 onClick={handleFinish}>
+                    Start
+                    </button>
+              </form>)}
+             
           </div>
           </div>
           );
